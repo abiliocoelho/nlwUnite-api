7d3774799825a8d4becc1bfcbd35b300c32f1b12
Abílio Soares Coelho <abiliocoelho@gmail.com>
2024-04-03 13:40:13 -0300

    :fire: Feat: lesson 3

--- src/routes/get_attendee-badge.ts
+++ src/routes/get_attendee-badge.ts
@@ -10,6 +10,16 @@ export async function getAttendeeBadge(app: FastifyInstance) {
         params: z.object({
           attendeeId: z.coerce.number(),
         }),
+        response: {
+          200: z.object({
+            badge: z.object({
+              name: z.string(),
+              email: z.string().email(),
+              eventTitle: z.string(),
+              checkInURL: z.string().url(),
+            }),
+          }),
+        },
       },
     },
     async (request, reply) => {
@@ -32,7 +42,18 @@ export async function getAttendeeBadge(app: FastifyInstance) {
         throw new Error('Attendee not found')
       }
 
-      return reply.send({ attendee })
+      const baseUrl = `${request.protocol}://${request.hostname}`
+
+      const checkInURL = new URL(`/attendees/${attendeeId}/check-in`, baseUrl)
+
+      return reply.send({
+        badge: {
+          name: attendee.name,
+          email: attendee.email,
+          eventTitle: attendee.event.title,
+          checkInURL: checkInURL.toString(),
+        },
+      })
     },
   )
 }
--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,9 @@ import {
   serializerCompiler,
   validatorCompiler,
 } from 'fastify-type-provider-zod'
+import { CheckIn } from './routes/check-in'
 import { createEvent } from './routes/create-event'
+import { getEventAttendees } from './routes/get-event-attendees'
 import { getEvent } from './routes/get-events'
 import { getAttendeeBadge } from './routes/get_attendee-badge'
 import { registerForEvent } from './routes/register-for-events'
@@ -14,5 +16,7 @@ app.register(createEvent)
 app.register(registerForEvent)
 app.register(getEvent)
 app.register(getAttendeeBadge)
+app.register(CheckIn)
+app.register(getEventAttendees)
 
 app.listen({ port: 3333 }).then(() => console.log('server running...'))
